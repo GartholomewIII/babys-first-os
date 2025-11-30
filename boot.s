b647e30b8ceeb81a7805d7f91f6bc2ce97e80627
Quinn Olney <quinnolney123@gmail.com>
2025-11-30 13:14:23 -0700

    building bootloader

---
+++ boot.s
@@ -0,0 +1,33 @@
+.set ALIGN, 1<<0
+.set MEMINFO, 1<<1
+.set FLAGS, ALIGN | MEMINFO #Multiboot flag field
+.set MAGIC, 0x1BADB002 #lets GRUB bootloader find header
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384 #16 KiB
+stack_top:
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+
+    mov $stack_top, %esp
+
+    call kernal_main
+
+    cli
+    hlt
+    jmp 1b
+
+.size _start, . - _start
+
